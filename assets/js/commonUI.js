8d9cce3252516be33f1f95fd8f46a5e4365282cf
wangyi73277 <46939811+wangyi73277@users.noreply.github.com>
2025-07-18 20:46:17 -0400

    add commonUI.js

---
+++ assets/js/commonUI.js
@@ -0,0 +1,20 @@
+function closeInfoPanel() {
+    document.getElementById('info-panel').style.display = 'none';
+    window.location.href = '../index.html';
+}
+
+function showInfo() {
+    document.getElementById('infoModal').style.display = 'block';
+}
+
+function closeInfo() {
+    document.getElementById('infoModal').style.display = 'none';
+}
+
+function showStory() {
+    document.getElementById('storyModal').style.display = 'block';
+}
+
+function closeStory() {
+    document.getElementById('storyModal').style.display = 'none';
+}
